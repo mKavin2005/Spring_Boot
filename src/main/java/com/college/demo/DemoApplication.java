dff7d782820a444dc710657094240bb645f0e412
Your Name <you@example.com>
2025-03-25 09:58:32 +0530

    all files

--- src/main/java/com/college/demo/DemoApplication.java
+++ src/main/java/com/college/demo/DemoApplication.java
@@ -1,25 +1,14 @@
 package com.college.demo;
 
-
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import com.college.demo.entity.User;
-import java.util.List;
-import java.util.ArrayList;
-
-@Service
-public class UserService {
-package com.college.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-	@SpringBootApplication
-	public class DemoApplication {
+@SpringBootApplication
+public class DemoApplication {
 
-		public static void main(String[] args) {
-			SpringApplication.run(DemoApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
 
-		}
+    }
 
-	}}
+}
